d6540a1efb064bf4e220a5b68f7d293fa682e3a7
FES001 <firdavs7794948@gmail.com>
2025-03-27 04:00:50 +0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -9,9 +9,16 @@ for (let i = 0; i < 7; i++) {
     for (let iterator = 0; iterator < 6; iterator++) {
         const columnElementChild = document.createElement('div');
         columnElementChild.classList.add('main-itself-inner-child');
-
+        columnElementChild.setAttribute('data-child-state', 'empty');
         columnElement.appendChild(columnElementChild);
     };
-
     mainItself.appendChild(columnElement);
+};
+
+const columnElement = mainItself.querySelectorAll('.main-itself-inner');
+
+for (let i = 0; i < columnElement.length; i++) {
+    columnElement[i].addEventListener('click', () => {
+        console.log(columnElement[i]);
+    });
 };
