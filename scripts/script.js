4745184f15b70f33b9c151873d0b7164f7efa5eb
FES001 <firdavs7794948@gmail.com>
2025-03-27 20:26:50 +0500

    Working on it.

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,33 @@
 const mainItself = document.querySelector('.main-itself');
 
+// GAME OBJECT
+
+// TURN
+const turn = {
+    isBluesTurn: false,
+    isClass: 'main-itself-inner-child-red-occupied',
+    isState: 'red-occupied',
+}
+
+// CHANGE TURNS
+
+function changeTurns() {
+    if (turn.isBluesTurn === false) {
+        turn.isClass = 'main-itself-inner-child-red-occupied';
+        turn.isState = 'red-occupied';
+    } else {
+        turn.isClass = 'main-itself-inner-child-blue-occupied';
+        turn.isState = 'blue-occupied';
+    };
+};
+
 // ADDING THE ELEMENTS
 
 for (let i = 0; i < 7; i++) {
     const columnElement = document.createElement('div');
     columnElement.classList.add('main-itself-inner');
+
+    columnElement.addEventListener('click', () => handlingTheColumnElement(columnElement));
 
     for (let iterator = 0; iterator < 6; iterator++) {
         const columnElementChild = document.createElement('div');
@@ -15,10 +38,18 @@ for (let i = 0; i < 7; i++) {
     mainItself.appendChild(columnElement);
 };
 
-const columnElement = mainItself.querySelectorAll('.main-itself-inner');
+// HANDLING THE COLUMN ELEMENT
 
-for (let i = 0; i < columnElement.length; i++) {
-    columnElement[i].addEventListener('click', () => {
-        console.log(columnElement[i]);
-    });
+function handlingTheColumnElement(columnElement) {
+    const columnChildren = columnElement.querySelectorAll('.main-itself-inner-child');
+
+    for (let i = 5; i >= 0; i--) {
+        const columnChildState = columnChildren[i].getAttribute('data-child-state');
+
+        if (columnChildState === 'empty') {
+            columnChildren[i].classList.add(turn.isClass);
+            columnChildren[i].setAttribute('data-child-state', turn.isState);
+            break;
+        };
+    };
 };
