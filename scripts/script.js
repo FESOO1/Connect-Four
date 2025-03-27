2f8712e955cfa0ad438b36e63caf4a4a21798681
FES001 <firdavs7794948@gmail.com>
2025-03-27 21:08:48 +0500

    Working on it.

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,14 @@
 const mainItself = document.querySelector('.main-itself');
 
-// GAME OBJECT
+// GAME
+let gameHasBeenWon = false;
 
 // TURN
 const turn = {
     isBluesTurn: false,
     isClass: 'main-itself-inner-child-red-occupied',
     isState: 'red-occupied',
+    winnerClass: 'main-itself-inner-child-red-winner',
 }
 
 // CHANGE TURNS
@@ -15,9 +17,11 @@ function changeTurns() {
     if (turn.isBluesTurn === false) {
         turn.isClass = 'main-itself-inner-child-red-occupied';
         turn.isState = 'red-occupied';
+        turn.winnerClass = 'main-itself-inner-child-red-winner';
     } else {
         turn.isClass = 'main-itself-inner-child-blue-occupied';
         turn.isState = 'blue-occupied';
+        turn.winnerClass = 'main-itself-inner-child-blue-winner';
     };
 };
 
@@ -33,6 +37,7 @@ for (let i = 0; i < 7; i++) {
         const columnElementChild = document.createElement('div');
         columnElementChild.classList.add('main-itself-inner-child');
         columnElementChild.setAttribute('data-child-state', 'empty');
+        columnElementChild.setAttribute('data-child-index', iterator);
         columnElement.appendChild(columnElementChild);
     };
     mainItself.appendChild(columnElement);
@@ -46,10 +51,79 @@ function handlingTheColumnElement(columnElement) {
     for (let i = 5; i >= 0; i--) {
         const columnChildState = columnChildren[i].getAttribute('data-child-state');
 
-        if (columnChildState === 'empty') {
-            columnChildren[i].classList.add(turn.isClass);
-            columnChildren[i].setAttribute('data-child-state', turn.isState);
-            break;
+        if (turn.isBluesTurn === false) {
+            if (columnChildState === 'empty') {
+                columnChildren[i].classList.add(turn.isClass);
+                columnChildren[i].setAttribute('data-child-state', turn.isState);
+                // CHECKING IF THE GAME HAS BEEN WON
+                checkIfTheGamehasBeenWon();
+                
+                if (gameHasBeenWon === false) {
+                    turn.isBluesTurn = true;
+                    changeTurns();
+                };
+                break;
+            };
+        } else if (turn.isBluesTurn === true) {
+            if (columnChildState === 'empty') {
+                columnChildren[i].classList.add(turn.isClass);
+                columnChildren[i].setAttribute('data-child-state', turn.isState);
+                // CHECKING IF THE GAME HAS BEEN WON
+                checkIfTheGamehasBeenWon();
+
+                if (gameHasBeenWon === false) {
+                    turn.isBluesTurn = false;
+                    changeTurns();
+                };
+                break;
+            };
+        };
+    };
+};
+
+// CHECK IF THE GAME HAS BEEN WON
+
+function checkIfTheGamehasBeenWon() {
+    const columnElements = mainItself.querySelectorAll('.main-itself-inner');
+    const elementStates = {
+        elementState: [],
+        elementIndex: [],
+        elementCounter: 0,
+        parentIndex: 0,
+    };
+
+    // CHECKING VERTICALLY
+    for (let i = 0; i < columnElements.length; i++) {
+        for (let iterator = 0; iterator < columnElements[i].childElementCount; iterator++) {
+            const childState = columnElements[i].children[iterator].getAttribute('data-child-state');
+            const childIndex = columnElements[i].children[iterator].getAttribute('data-child-index');
+
+            elementStates.elementState.push(childState);
+            elementStates.elementIndex.push(childIndex);
+            elementStates.parentIndex = i;
+
+            if (elementStates.elementState.length === 6) {
+
+                // CHECKING IF FOUR ELEMENTS HAVE BEEN OCCUPIED
+                for (const state of elementStates.elementState) {
+                    if (state === turn.isState) {
+                        elementStates.elementCounter++;
+
+                        if (elementStates.elementCounter === 4) {
+                            const winner = turn.isBluesTurn === false ? 'Red One the Game' : 'Blue Won the Game';
+
+                            columnElements[i].children[iterator].classList.add(turn.winnerClass);
+                            console.log(elementStates.elementIndex);
+                            return;
+                        };
+                    } else {
+                        elementStates.elementState = [];
+                        elementStates.elementIndex = [];
+                        elementStates.parentIndex = 0;
+                        elementStates.elementCounter = 0;
+                    };
+                };
+            };
         };
     };
 };
